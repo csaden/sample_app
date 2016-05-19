b20335e4508b28bb1a65d7d165d6ff400ae32b1e
Chris Saden <chris.saden@udacity.com>
2016-05-19 11:23:10 -0700

    solves 5.6 exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,5 +13,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", full_title("Sign up")
   end
+
 end
