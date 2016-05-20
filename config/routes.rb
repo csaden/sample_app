3363d46f18db62e615cab766504b04385d74b495
Chris Saden <chris.saden@udacity.com>
2016-05-20 01:37:37 -0700

    add edit and update for users and initial auth

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
   get    'signup'  => 'users#new'
+  get    'edit'    => 'users#edit'
+  patch  'update'  => 'users#update'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
